99e3fbce4dc260ca3f67772a5ceb6446023f5056
Gusarov2906 <gusarov2906@gmail.com>
2021-03-28 22:41:22 +0300

    Added Welcome widget ui

--- MyEverydayApp/src/mainwindow.cpp
+++ MyEverydayApp/src/mainwindow.cpp
@@ -1,13 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "WeatherTodayWidget.h"
+#include "WelcomeWidget.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);    
-    QVBoxLayout* lay = new QVBoxLayout(ui->widget01);
-    lay->addWidget(new WeatherTodayWidget(ui->widget01));
+    ui->setupUi(this);
+    QVBoxLayout* lay00 = new QVBoxLayout(ui->widget00);
+    lay00->addWidget(new WelcomeWidget(ui->widget00));
+    QVBoxLayout* lay01 = new QVBoxLayout(ui->widget01);
+    lay01->addWidget(new WeatherTodayWidget(ui->widget01));
 }
 
 MainWindow::~MainWindow()
