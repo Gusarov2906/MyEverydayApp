bada4a3a61bf80dac75c5b7ffc186523229804a0
Gusarov2906 <gusarov2906@gmail.com>
2021-04-16 08:52:55 +0300

    Added getters to weather today data

--- MyEverydayApp/src/weathertodaydata.cpp
+++ MyEverydayApp/src/weathertodaydata.cpp
@@ -22,3 +22,57 @@ WeatherTodayData::WeatherTodayData(QString curLocation, QTime curTime, Temperatu
     this->temperatureAt15 = temperatureAt15;
     this->temperatureAt21 = temperatureAt21;
 }
+
+QString WeatherTodayData::getCurLocation()
+{
+    return this->curLocation;
+}
+
+QTime WeatherTodayData::getCurTime()
+{
+    return this->curTime;
+}
+
+Temperature WeatherTodayData::getCurTemperature()
+{
+    return this->curTemperature;
+}
+
+QString WeatherTodayData::getDescription()
+{
+    return this->description;
+}
+
+Temperature WeatherTodayData::getFeelingTemperature()
+{
+    return this->feelingTemperature;
+}
+
+QString WeatherTodayData::getHumidity()
+{
+    return this->humidity;
+}
+QString WeatherTodayData::getPreassure()
+{
+    return this->preassure;
+}
+
+Temperature WeatherTodayData::getTemperatureAt3()
+{
+    return this->temperatureAt3;
+}
+
+Temperature WeatherTodayData::getTemperatureAt9()
+{
+    return this->temperatureAt9;
+}
+
+Temperature WeatherTodayData::getTemperatureAt15()
+{
+    return this->temperatureAt15;
+}
+
+Temperature WeatherTodayData::getTemperatureAt21()
+{
+    return this->temperatureAt21;
+}
